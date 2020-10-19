1c4a3e44170cb86668828f51dcb454824dbb6dc8
KeitoSekine <31131@ojs.ac.jp>
2020-10-19 13:35:30 +0900

    問題7-2

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -13,74 +13,70 @@ namespace Chapter7
     {
         static void Main(string[] args)
         {
-            var text = "Cozy lummox gives smart squid who asks for job pen";
+            // コンストラクタ呼び出し
+            var abbrs = new Abbreviations();
 
-            Exercise1_1(text);  //問題7-1-1
+            // Addメソッドの呼び出し例
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
+
+            //7-2-2
+            //Countプロパティを呼び出して数を出力させる
+            Console.WriteLine(abbrs.Count);
+
+            //7-2-3
+            //Removeプロパティを呼び出して出力させる
+            if (abbrs.Remove("IOC"))
+            {
+                Console.WriteLine("削除成功");
+            }
+            else
+            {
+                Console.WriteLine("削除失敗");
+            }
+
+            //7-2-4
+            //3文字の省略語だけ取り出す
+            foreach (var item in abbrs.FindAll("国際"))
+            {
+                Console.WriteLine($"{item.Key}={item.Value}");
+            }
+            Console.WriteLine();
+            foreach (var item in abbrs.Where(w => w.Key.Length == 3))
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+            
+
+
+            // インデクサの利用例
+            //var names = new[] { "WHO", "FIFA", "NPT", };
+            //foreach (var name in names)
+            //{
+            //    var fullname = abbrs[name];
+            //    if (fullname == null)
+            //        Console.WriteLine("{0}は見つかりません", name);
+            //    else
+            //        Console.WriteLine("{0}={1}", name, fullname);
+            //}
+            //Console.WriteLine();
+
+            //// ToAbbreviationメソッドの利用例
+            //var japanese = "東南アジア諸国連合";
+            //var abbreviation = abbrs.ToAbbreviation(japanese);
+            //if (abbreviation == null)
+            //    Console.WriteLine("{0} は見つかりません", japanese);
+            //else
+            //    Console.WriteLine("「{0}」の略語は {1} です", japanese, abbreviation);
+            //Console.WriteLine();
+
+            //// FindAllメソッドの利用例
+            //foreach (var item in abbrs.FindAll("国際"))
+            //{
+            //    Console.WriteLine("{0}={1}", item.Key, item.Value);
+            //}
+            //Console.WriteLine();
         }
 
-        #region 問題7-1-1
-        //static void Exercise1_1(string text)
-        //{
-        //    var dict = new Dictionary<char, int>();
-        //    //foreachで一文字ずつ取り出す
-        //    //半角アルファベットが調べる
-        //    foreach (var ch in text)
-        //    {
-        //        //大文字に変換
-        //        char chars = char.ToUpper(ch);
-        //        if ('A' <= chars && chars <= 'Z')
-        //        {
-        //            //Keyに存在するか
-        //            if (dict.ContainsKey(chars))
-        //            {
-        //                //登録済み  
-        //                dict[chars]++;
-        //            }
-        //            else
-        //            {
-        //                //未登録
-        //                dict[chars] = 1;
-        //            }
-        //        }
-
-        //    }
-        //    foreach (var item in dict.OrderBy(o => o.Key))
-        //    {
-        //        Console.WriteLine($"{item.Key}：{item.Value}");
-        //    }
-        //}
-        #endregion
-        #region 問題7-1-2
-        static void Exercise1_1(string text)
-        {
-            var dict = new SortedDictionary<char, int>();
-            //foreachで一文字ずつ取り出す
-            //半角アルファベットが調べる
-            foreach (var ch in text)
-            {
-                //大文字に変換
-                char chars = char.ToUpper(ch);
-                if ('A' <= chars && chars <= 'Z')
-                {
-                    //Keyに存在するか
-                    if (dict.ContainsKey(chars))
-                    {
-                        //登録済み  
-                        dict[chars]++;
-                    }
-                    else
-                    {
-                        //未登録
-                        dict[chars] = 1;
-                    }
-                }
-
-            }
-            foreach (var item in dict)
-            {
-                Console.WriteLine($"{item.Key}：{item.Value}");
-            }
-        }
-        #endregion
     }
 }
