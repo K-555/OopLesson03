5d07fe299bc215dde0a32d0db2b8e6398b19002d
KeitoSekine <31131@ojs.ac.jp>
2020-10-30 15:33:38 +0900

    SendMailApp新規作成

--- Chapter6/Program.cs
+++ Chapter6/Program.cs
@@ -17,19 +17,19 @@ namespace Chapter6
             //問題6-1-1
             //Console.WriteLine("問題6-1-1");
             //Console.WriteLine(numbers.Max());
-            ////問題6-1-2
+            //問題6-1-2
             //Console.WriteLine("問題6-1-2");
             //int pos = numbers.Length - 2;
             //numbers.Skip(pos).ToList().ForEach(f => Console.Write(f + " "));
-            ////問題6-1-3
+            //問題6-1-3
             //Console.WriteLine();
             //Console.WriteLine("問題6-1-3");
             //numbers.Select(s => s.ToString()).ToList().ForEach(f => Console.Write(f + " "));
-            ////問題6-1-4
+            //問題6-1-4
             //Console.WriteLine();
             //Console.WriteLine("問題6-1-4");
             //numbers.OrderBy(o => o).Take(3).ToList().ForEach(f => Console.Write(f + " "));
-            ////問題6-1-5
+            //問題6-1-5
             //Console.WriteLine();
             //Console.WriteLine("問題6-1-5");
             //Console.WriteLine(numbers.Distinct().Count(c => c > 10));
@@ -38,68 +38,68 @@ namespace Chapter6
             #region 問題6-2
             //問題6-2-1
 
-            //Console.WriteLine("問題6-2-1");
-            //var book = books.Where(f => f.Title == "ワンダフル・C#ライフ");
-            ////var book = books.FirstOrDefault(f => f.Title == "ワンダフル・C#ライフ");
+            Console.WriteLine("問題6-2-1");
+            var book = books.Where(f => f.Title == "ワンダフル・C#ライフ");
+            //var book = books.FirstOrDefault(f => f.Title == "ワンダフル・C#ライフ");
 
-            //foreach (var item in book)
-            //{
-            //    Console.WriteLine($"価格:{item.Price} ページ数:{item.Pages}");
-            //}
+            foreach (var item in book)
+            {
+                Console.WriteLine($"価格:{item.Price} ページ数:{item.Pages}");
+            }
 
-            ////問題6-2-2
-            //Console.WriteLine("問題6-2-2");
-            //var selected = books.Count(c => c.Title.Contains("C#"));
-            //Console.WriteLine($"{selected}冊");
+            //問題6-2-2
+            Console.WriteLine("問題6-2-2");
+            var selected = books.Count(c => c.Title.Contains("C#"));
+            Console.WriteLine($"{selected}冊");
 
-            ////問題6-2-3
-            //Console.WriteLine("問題6-2-3");
-            //var sa = books.Where(c => c.Title.Contains("C#")).Average(a=>a.Pages);
-            //Console.WriteLine(sa);
+            //問題6-2-3
+            Console.WriteLine("問題6-2-3");
+            var sa = books.Where(c => c.Title.Contains("C#")).Average(a => a.Pages);
+            Console.WriteLine(sa);
 
-            ////問題6-2-4
-            //Console.WriteLine("問題6-2-4");
-            //var bookp = books.Where(w => w.Price >= 4000);
-            //foreach (var bookps in bookp)
-            //{
-            //    Console.WriteLine(bookps.Title);
-            //}
+            //問題6-2-4
+            Console.WriteLine("問題6-2-4");
+            var bookp = books.Where(w => w.Price >= 4000);
+            foreach (var bookps in bookp)
+            {
+                Console.WriteLine(bookps.Title);
+            }
 
-            ////問題6-2-5
-            //Console.WriteLine("問題6-2-5");
-            //var pt = books.Where(w => w.Price < 4000).Max(m => m.Pages);
-            //Console.WriteLine(pt);
+            //問題6-2-5
+            Console.WriteLine("問題6-2-5");
+            var pt = books.Where(w => w.Price < 4000).Max(m => m.Pages);
+            Console.WriteLine(pt);
 
-            ////問題6-2-6
-            //Console.WriteLine("問題6-2-6");
-            //var bookpt = books.Where(w => w.Pages >= 400).OrderByDescending(o => o.Price);
-            //foreach (var bookpts in bookpt)
-            //{
-            //    Console.WriteLine($"{bookpts.Title}　{bookpts.Price}");
-            //}
+            //問題6-2-6
+            Console.WriteLine("問題6-2-6");
+            var bookpt = books.Where(w => w.Pages >= 400).OrderByDescending(o => o.Price);
+            foreach (var bookpts in bookpt)
+            {
+                Console.WriteLine($"{bookpts.Title}　{bookpts.Price}");
+            }
 
-            ////問題6-2-7
-            //Console.WriteLine("問題6-2-7");
-            //var bookppt = books.Where(w => w.Title.Contains("C#") && w.Pages <= 500);
-            //foreach (var bookppts in bookppt)
-            //{
-            //    Console.WriteLine(bookppts.Title);
-            //}
+            //問題6-2-7
+            Console.WriteLine("問題6-2-7");
+            var bookppt = books.Where(w => w.Title.Contains("C#") && w.Pages <= 500);
+            foreach (var bookppts in bookppt)
+            {
+                Console.WriteLine(bookppts.Title);
+            }
             #endregion
 
             //すべての書籍から、「C#」の文字がいくつあるかをカウントする
-            int count = 0;
-            foreach (var book in books.Where(b => b.Title.Contains("C#")))
-            {
-                for (int i = 0; i < book.Title.Length-1; i++)
-                {
-                    if ((book.Title[i] == 'C')&&(book.Title[i+1]=='#'))
-                    {
-                        count++;
-                    }
-                }
-            }
-            Console.WriteLine($"文字列「C#」の個数は{count}です。");
+            //int count = 0;
+            //foreach (var book in books.Where(b => b.Title.Contains("C#")))
+            //{
+            //    for (int i = 0; i < book.Title.Length-1; i++)
+            //    {
+            //        if ((book.Title[i] == 'C')&&(book.Title[i+1]=='#'))
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine($"文字列「C#」の個数は{count}です。");
         }
     }
 }
