1b1f5a83cf7f4ad2326eab4cfa7efa123e58c4c3
KeitoSekine <31131@ojs.ac.jp>
2020-11-05 10:20:55 +0900

    複数送信作成中

--- SendMailApp/MainWindow.xaml.cs
+++ SendMailApp/MainWindow.xaml.cs
@@ -49,9 +49,14 @@ namespace SendMailApp
             try
             {
                 MailMessage msg = new MailMessage("ojsinfosys01@gmail.com" , tbTo.Text);
-
-                msg.CC.Add(tbCc.Text);
-                
+                if (tbCc.Text != "")
+                {
+                    msg.CC.Add(tbCc.Text);
+                }
+                else if (tbBcc.Text != "")
+                {
+                    msg.Bcc.Add(tbBcc.Text);
+                }
                 msg.Subject = tbTitle.Text; //件名
                 msg.Body = tbBody.Text; //本文
 
